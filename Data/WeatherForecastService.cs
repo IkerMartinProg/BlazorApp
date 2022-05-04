72bcbf1eef6988a18f54d9e4708a38d33983b1df
IkerMartin <ikermartinprog@gmail.com>
2022-05-04 13:30:40 +0200

    Versión con comentarios

--- Data/WeatherForecastService.cs
+++ Data/WeatherForecastService.cs
@@ -2,13 +2,22 @@ namespace BlazorApp.Data;
 
 public class WeatherForecastService
 {
+    /*
+        Array "Summaries": contiene los diferentes tipos de estado temporal.
+    */
     private static readonly string[] Summaries = new[]
     {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
-
+    
     public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
     {
+        /* 
+            Devuelve cinco filas de datos (Range).
+            Los datos recogidos se asocian a la clase de definición "WeatherForecast".
+            Los valores de "TemperatureC" y "Summary" se generan de manera random, 
+            respetando el rango de valores establecido.
+        */
         return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
             Date = startDate.AddDays(index),
